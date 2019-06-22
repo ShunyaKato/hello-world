6da895ca5cfc585fb829bc9ffddbde8c057c9244
ShunyaKato <shun.k0829@gmail.com>
2019-06-23 03:40:22 +0900

    add logo fadein/out

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,4 +1,29 @@
 "use strict";
 
-var a = "aa";
-console.log("\u30D9\u30D3\u30FC\u308F\u3093\u3053");
+//スクロールするたびに処理はしらす
+//yをいい感じの値に調整
+//CSSをつける
+var headerlogo = document.getElementById("headerlogo");
+var changePosition = 100;
+var isTop = window.pageYOffset <= changePosition ? true : false;
+
+window.onscroll = function () {
+  console.log(isTop);
+  var y = window.pageYOffset;
+
+  if (y <= changePosition) {
+    if (isTop === false) {
+      isTop = true;
+      headerlogo.classList.add("fadeout");
+      headerlogo.classList.remove("fadein");
+    }
+
+    return false;
+  }
+
+  if (isTop === true) {
+    isTop = false;
+    headerlogo.classList.remove("fadeout");
+    headerlogo.classList.add("fadein");
+  }
+};
--- frontend/babel/index.js
+++ frontend/babel/index.js
@@ -1,3 +1,24 @@
-let a = "aa";
+//スクロールするたびに処理はしらす
+//yをいい感じの値に調整
+//CSSをつける
+const headerlogo = document.getElementById("headerlogo");
+const changePosition = 100;
+let isTop = window.pageYOffset <= changePosition ? true : false;
 
-console.log(`ベビーわんこ`);
+window.onscroll = () => {
+  console.log(isTop);
+  let y = window.pageYOffset;
+  if (y <= changePosition) {
+    if (isTop === false) {
+      isTop = true;
+      headerlogo.classList.add("fadeout");
+      headerlogo.classList.remove("fadein");
+    }
+    return false;
+  }
+  if (isTop === true) {
+    isTop = false;
+    headerlogo.classList.remove("fadeout");
+    headerlogo.classList.add("fadein");
+  }
+}
