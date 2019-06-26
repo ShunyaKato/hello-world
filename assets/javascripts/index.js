d7f557114fbac7d9cadba0a8178ebdcd2e69f12c
ShunyaKato <shun.k0829@gmail.com>
2019-06-27 00:37:32 +0900

    fix carList css

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -3,26 +3,44 @@
 //スクロールするたびに処理はしらす
 //yをいい感じの値に調整
 //CSSをつける
-var headerlogo = document.getElementById("headerlogo");
-var changePosition = 100;
-var isTop = window.pageYOffset <= changePosition ? true : false;
+window.onload = function () {
+  var headerlogo = document.getElementById("headerlogo");
+  var changePosition = 100;
+  var isTop = window.pageYOffset <= changePosition ? true : false;
+  console.log(isTop);
 
-window.onscroll = function () {
-  var y = window.pageYOffset;
+  var hideLogo = function hideLogo() {
+    headerlogo.classList.add("fadeout");
+    headerlogo.classList.remove("fadein");
+  };
 
-  if (y <= changePosition) {
-    if (isTop === false) {
-      isTop = true;
-      headerlogo.classList.add("fadeout");
-      headerlogo.classList.remove("fadein");
-    }
-
-    return false;
-  }
-
-  if (isTop === true) {
-    isTop = false;
+  var showLogo = function showLogo() {
     headerlogo.classList.remove("fadeout");
     headerlogo.classList.add("fadein");
+  }; // if (isTop === true) {
+  //   headerlogo.style.display = "none";
+  // }
+
+
+  if (isTop === false) {
+    showLogo();
   }
+
+  window.onscroll = function () {
+    var y = window.pageYOffset;
+
+    if (y <= changePosition) {
+      if (isTop === false) {
+        isTop = true;
+        hideLogo();
+      }
+
+      return false;
+    }
+
+    if (isTop === true) {
+      isTop = false;
+      showLogo();
+    }
+  };
 };
